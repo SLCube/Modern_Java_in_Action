bdd24547bfecb3c17a470a5fce7c2bdcddcf0785
SLCube <kgm0096@naver.com>
2023-06-07 15:35:16 +0900

    anonymous class 사용

---
+++ src/ch1/anonymous_class/Main.java
@@ -0,0 +1,32 @@
+package ch1.anonymous_class;
+
+import ch1.common.Apple;
+import ch1.common.ApplePredicate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Apple> inventory = new ArrayList<>();
+
+        List<Apple> apples = filterApples(inventory, new ApplePredicate() {
+            @Override
+            public boolean test(Apple apple) {
+                /**
+                 * anonymous class를 이용해 class파일을 만들지 않아도 새로운 기능을 추가 할 수 있게 됐지만,
+                 * method의 argument를 전달하는 공간이 너무 비대해졌다.
+                 *
+                 */
+                return false;
+            }
+        });
+    }
+
+    public static List<Apple> filterApples(List<Apple> apples, ApplePredicate predicate) {
+        List<Apple> result = new ArrayList<>();
+
+        return result;
+    }
+}
